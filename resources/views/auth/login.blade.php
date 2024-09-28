d36ffccdec3dc82e2c706d17c2786437c7d40230
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-09-28 18:08:02 +0700

    feat(login): create ui login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,27 @@
 <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <div class="w-full">
+        <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+       <div class=" p-6 rounded-lg w-120">
+            <h1 class="text-5xl font-bold text-left mt-8 pt-2 mb-2 text-customColor">Masuk</h1>
+            <h4 class="mb-20 font-medium">Silahkan ketikan username dan passwordmu di bawah ini</h4>
+            <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="-mt-10 mr-14">
+                <x-text-input id="username" class="w-full p-3 border-2 border-green-500 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 mb-6" type="username" name="username" :value="old('username')" required autofocus autocomplete="username" placeholder="Username"/>
+                <x-input-error :messages="$errors->get('username')" class="mt-2" />
+            </div>
 
-        <!-- username Address -->
-        <div>
-            <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" class="block mt-1 w-full" type="username" name="username" :value="old('Username')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('username')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+            <div class="mr-14">
+                <x-text-input id="password" class="w-full p-3 border-2 border-green-500 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 mb-6" type="password" name="password" required autocomplete="current-password" placeholder="Password"/>
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+            <div class="mr-14">
+                <button class="w-full bg-green-500 text-white text-center font-semibold py-2 rounded hover:bg-green-600 transition duration-300 mb-20">
+                        {{ __('Masuk') }}
+                </button>
+            </div>
+        </form>
+       </div>
+    </div>
 </x-guest-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["DM Sans", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                customColor: "#2b2b2b",
+                whiteColor: "#f1f1f1",
+                primaryColor: "#00b21d",
+                darkPrimaryColor: "#007713",
+                lightColor: "#00c620",
             },
         },
     },
