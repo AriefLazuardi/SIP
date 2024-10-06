a425215b07907d639b854c8b640f83935222586d
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-10-06 22:32:20 +0700

    Create confirm-alert.blade.php

---
+++ resources/views/components/confirm-alert.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <script>
+        function confirmDelete(route) {
+            Swal.fire({
+                text: 'Apakah Anda yakin ingin menghapus akun ini?',
+                icon: 'warning',
+                showDenyButton: true,
+                confirmButtonText: 'Hapus',
+                denyButtonText: 'Batal',
+                customClass: {
+                    denyButton: 'bg-primaryColor text-white px-24 py-2 rounded-md ml-2',
+                    confirmButton: 'bg-white border-2 border-dangerColor text-dangerColor px-24 py-2 rounded-md mr-2',
+                    popup: 'flex flex-col items-center'
+                },
+                buttonsStyling: false,
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Submit form with the given route
+                    document.getElementById(route).submit();
+                }
+            });
+        }
+    </script>
+</div>
