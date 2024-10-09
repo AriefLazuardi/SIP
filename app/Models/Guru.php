c8f64b84f34d47315a6305dc19dacb78fbfe399f
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-10-10 00:09:06 +0700

    Create Guru.php

---
+++ app/Models/Guru.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laratrust\Models\Role as RoleUser;
+
+class Guru extends Model
+{
+    protected $table = 'guru';
+    protected $fillable = [
+        'name',              
+        'nip',               
+        'total_jam_perminggu', 
+        'user_id',     
+    ];
+    use HasFactory;
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function waliKelas()
+    {
+        return $this->belongsTo(WaliKelas::class);
+    }
+}
