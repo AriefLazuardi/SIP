3b34672f8d968991268c372cd29983807dd3f559
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-10-10 20:27:59 +0700

    Update RouteHelper.php

--- app/Helpers/RouteHelper.php
+++ app/Helpers/RouteHelper.php
@@ -1,18 +1,47 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
 
 if (!function_exists('getDashboardRoute')) {
     function getDashboardRoute()
     {
-        if (auth()->user()->hasRole('administrator')) {
+        if (Auth::user()->hasRole('administrator')) {
             return 'admin.dashboard';
-        } elseif (auth()->user()->hasRole('guru')) {
-            return 'guru.dashboard';
-        } elseif (auth()->user()->hasRole('wakakurikulum')) {
+        } elseif (Auth::user()->hasRole('wakakurikulum')) {
             return 'wakilkurikulum.dashboard';
+        } elseif (Auth::user()->hasRole('guru')) {
+            return 'guru.dashboard';
         }
         
         // Default route jika tidak ada yang cocok
         return 'dashboard';
+    }
+}
+
+if (!function_exists('setActiveClass')) {
+    function setActiveClass($menu, $activeClass = 'bg-primaryColor text-white', $inactiveClass = 'bg-baseColor text-customColor')
+    {
+        // Definisikan routes yang terkait dengan setiap menu
+        $routeMap = [
+            'guru' => ['wakilkurikulum.guru.index', 'wakilkurikulum.guru.edit', 'wakilkurikulum.guru.create'],
+            'mapel' => ['wakilkurikulum.mapel.index', 'wakilkurikulum.mapel.edit', 'wakilkurikulum.mapel.create'],
+            'kelas' => ['wakilkurikulum.kelas.index', 'wakilkurikulum.kelas.edit', 'wakilkurikulum.kelas.create'],
+            'ruangan' => ['wakilkurikulum.ruangan.index', 'wakilkurikulum.ruangan.edit', 'wakilkurikulum.ruangan.create'],
+            'hari' => ['wakilkurikulum.hari.index', 'wakilkurikulum.hari.edit', 'wakilkurikulum.hari.create'],
+            'tahunajaran' => ['wakilkurikulum.tahunajaran.index', 'wakilkurikulum.tahunajaran.edit', 'wakilkurikulum.tahunajaran.create'],
+            
+        ];
+
+        // Cek apakah menu ada di routeMap dan apakah route saat ini termasuk dalam array tersebut
+        if (array_key_exists($menu, $routeMap)) {
+            foreach ($routeMap[$menu] as $route) {
+                if (Route::currentRouteName() === $route) {
+                    return $activeClass;
+                }
+            }
+        }
+
+        return $inactiveClass;
     }
 }
