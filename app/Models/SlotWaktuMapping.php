c4e545eb9cea82fdc8dac415a14fcedc8c9d6611
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-11-04 17:15:24 +0700

    Create SlotWaktuMapping.php

---
+++ app/Models/SlotWaktuMapping.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SlotWaktuMapping extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $table = 'slot_waktu_mapping';
+    protected $fillable = [
+        'slot_waktu_tingkatan_kelas_id',
+        'hari_id',
+    ];
+
+    public function slotWaktuTingkatanKelas()
+    {
+        return $this->belongsTo(SlotWaktuTingkatanKelas::class, 'slot_waktu_tingkatan_kelas_id');
+    }
+
+    public function hari()
+    {
+        return $this->belongsTo(Hari::class, 'hari_id');
+    }
+}
