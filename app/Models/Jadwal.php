e37c56dfc595572190ed6840b4a57d24ac0377be
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-11-11 18:09:29 +0700

    Update Jadwal.php

--- app/Models/Jadwal.php
+++ app/Models/Jadwal.php
@@ -10,29 +10,29 @@ class Jadwal extends Model
     use HasFactory;
     protected $table = 'jadwal';
     protected $fillable = [
-        'tugas_mengajar_id',
+        'kelas_tugas_mengajar_id',
         'slot_waktu_mapping_id',
         'ruangan_id',
         'tahun_ajaran_id'
     ];
 
-    public function tugasMengajar()
+    public function kelasTugasMengajar()
     {
-        return $this->belongsTo(TugasMengajar::class);
+        return $this->belongsTo(KelasTugasMengajar::class, 'kelas_tugas_mengajar_id');
     }
 
     public function slotWaktuMapping()
     {
-        return $this->belongsTo(SlotWaktuMapping::class);
+        return $this->belongsTo(SlotWaktuMapping::class, 'slot_waktu_mapping_id');
     }
 
     public function ruangan()
     {
-        return $this->belongsTo(Ruangan::class);
+        return $this->belongsTo(Ruangan::class, 'ruangan_id');
     }
 
     public function tahunAjaran()
     {
-        return $this->belongsTo(TahunAjaran::class);
+        return $this->belongsTo(TahunAjaran::class, 'tahun_ajaran_id');
     }
 }
