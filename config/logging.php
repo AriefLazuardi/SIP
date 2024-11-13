f1d9619074a8d20586227c677b394737c91e8733
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-11-13 22:26:39 +0700

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -127,6 +127,13 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'penjadwalan' => [
+        'driver' => 'daily',
+        'path' => storage_path('logs/penjadwalan.log'),
+        'level' => 'debug',
+        'days' => 14,
+        ],
+
     ],
 
 ];
