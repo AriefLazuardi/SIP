5339abd52bcc90c893a16baeff9a3827644b3f01
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-11-17 21:17:37 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,16 +8,16 @@
             <form method="POST" action="{{ route('login') }}">
             @csrf
             <div class="-mt-10 mr-14">
-                <x-text-input id="username" class="w-full p-3 border-2 border-green-500 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 mb-6" type="username" name="username" :value="old('username')" required autofocus autocomplete="username" placeholder="Username"/>
+                <x-text-input id="username" class="w-full p-3 border-2 border-customColor rounded-md focus:outline-none  mb-6" type="username" name="username" :value="old('username')" required autofocus autocomplete="username" placeholder="Username"/>
                 <x-input-error :messages="$errors->get('username')" class="mt-2" />
             </div>
 
             <div class="mr-14">
-                <x-text-input id="password" class="w-full p-3 border-2 border-green-500 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 mb-6" type="password" name="password" required autocomplete="current-password" placeholder="Password"/>
+                <x-text-input id="password" class="w-full p-3 border-2 border-customColor rounded-md focus:outline-none  mb-6" type="password" name="password" required autocomplete="current-password" placeholder="Password"/>
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
             <div class="mr-14">
-                <button class="w-full bg-green-500 text-white text-center font-semibold py-2 rounded hover:bg-green-600 transition duration-300 mb-20">
+                <button class="w-full bg-green-500 text-white text-center font-semibold py-3 rounded hover:bg-green-600 transition duration-300 mb-20">
                         {{ __('Masuk') }}
                 </button>
             </div>
