e11ae0e0fce86e6c8689ff750fd6d448b96fd5e4
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-11-19 17:06:46 +0700

    Create detail-button.blade.php

---
+++ resources/views/components/detail-button.blade.php
@@ -0,0 +1,5 @@
+<a href="{{ $url }}" class="bg-skypeColor text-baseColor px-4 py-1 w-20 h-8 rounded-md flex items-center justify-center space-x-2 text-xs">
+    <span class="material-icons text-sm -translate-x-1">remove_red_eye</span>
+    {{ $slot}}
+</a>
+
