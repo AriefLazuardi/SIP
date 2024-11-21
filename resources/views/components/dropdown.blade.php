25803a1fbcd3d16540dc818390545c7e41902e00
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-11-21 15:13:04 +0700

    Update dropdown.blade.php

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,7 +1,19 @@
-@props(['options' => [], 'selected' => null, 'disabled' => false])
+@props(['options' => [], 'selected' => [], 'disabled' => false, 'placeholder' => 'Pilih disini', 'multiple' => false])
 
-<select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<select 
+    {{ $disabled ? 'disabled' : '' }} 
+    {!! $attributes->merge(['class' => 'border-baseColor focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}
+    {{ $multiple ? 'multiple' : '' }}
+>
+    <option value="" disabled {{ $selected === null ? 'selected' : '' }}>{{ $placeholder }}</option>
     @foreach($options as $value => $label)
-        <option value="{{ $value }}" {{ $value == $selected ? 'selected' : '' }}>{{ $label }}</option>
+        <option value="{{ $value }}" 
+            {{ $multiple 
+                ? (in_array($value, (array)$selected) ? 'selected' : '')
+                : ($value == $selected ? 'selected' : '') 
+            }}
+        >
+            {{ $label }}
+        </option>
     @endforeach
 </select>
