8ab3fa2e28dc272b8c96eca41a01dd1ea91acc21
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-11-29 21:41:33 +0700

    Update edit.blade.php

--- resources/views/admin/guru/edit.blade.php
+++ resources/views/admin/guru/edit.blade.php
@@ -26,6 +26,16 @@
                     <x-dropdown-custom name="user_id" :options="$users->pluck('username', 'id')" selected="{{ old('user_id', $guru->user_id ?? null)  }}" class="w-full" placeholder="Pilih User" />
                     <x-input-error :messages="$errors->get('user_id')" class="mt-2" />
                 </div>
+                <div class="mt-4">
+                    <x-input-label for="tempat_lahir" :value="__('Tempat Lahir')" />
+                     <x-text-input id="tempat_lahir" name="tempat_lahir" type="text" placeholder="Ketikkan disini" class="mt-1 block w-full" :value="old('tempat_lahir', $guru->tempat_lahir)"/>
+                     <x-input-error :messages="$errors->get('tempat_lahir')" class="mt-2" />
+                </div>
+                <div class="mt-4">
+                    <x-input-label for="jabatan" :value="__('Jabatan')" />
+                     <x-text-input id="jabatan" name="jabatan" type="text" placeholder="Ketikkan disini" class="mt-1 block w-full" :value="old('jabatan', $guru->jabatan)" />
+                     <x-input-error :messages="$errors->get('jabatan')" class="mt-2" />
+                </div>
 
             </div>
 
@@ -34,6 +44,16 @@
                     <x-input-label for="nip" :value="__('Nomor Identitas Pegawai')" />
                      <x-text-input id="nip" name="nip" type="text" placeholder="Ketikkan disini" class="mt-1 block w-full" :value="old('nip', $guru->nip)"/>
                      <x-input-error :messages="$errors->get('nip')" class="mt-2" />
+                </div>
+                <div class="mt-24">
+                    <x-input-label for="tanggal_lahir" :value="__('Tanggal Lahir')" />
+                    <x-date-input id="tanggal_lahir" name="tanggal_lahir" type="text" placeholder="Pilih Tanggal" class="mt-1 block w-full" required autofocus :value="old('tanggal_lahir', $guru->tanggal_lahir)" />
+                    <x-input-error :messages="$errors->get('tanggal_lahir')" class="mt-2" />
+                </div>
+                <div class="mt-4">
+                    <x-input-label for="golongan" :value="__('Golongan')" />
+                     <x-text-input id="golongan" name="golongan" type="text" placeholder="Ketikkan disini" class="mt-1 block w-full" :value="old('golongan', $guru->golongan)"/>
+                     <x-input-error :messages="$errors->get('golongan')" class="mt-2" />
                 </div>
             </div>
         </div>
@@ -53,6 +73,14 @@
         </div>
     </form>
 </div>
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+    flatpickr("#tanggal_lahir", {
+        dateFormat: "Y-m-d",
+    });
+});
+
+</script>
 @if (session('status') === 'guru-updated')
     <x-success-alert message="Berhasil mengubah data ini" icon="success" />
 @elseif (session('status') === 'error')
