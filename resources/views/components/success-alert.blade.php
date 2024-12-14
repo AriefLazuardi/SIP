fe1890d63c1dab9e4d9c5847208990e2875613da
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-12-14 18:25:22 +0700

    Update success-alert.blade.php

--- resources/views/components/success-alert.blade.php
+++ resources/views/components/success-alert.blade.php
@@ -1,15 +1,24 @@
-<div x-data="{
-    showAlert() {
-        Swal.fire({
-            text: '{{ $message ?? 'Operasi berhasil dilakukan.' }}',
-            icon: '{{ $icon ?? 'success' }}',
-            confirmButtonText: '{{ $confirmButtonText ?? 'OK' }}',
-            customClass: {
-                confirmButton: 'bg-primaryColor border-0 text-white px-4 py-2 rounded-md w-96'
-            },
-            buttonsStyling: false,
-        });
-    }
-}" 
-    x-init="showAlert()">
+@props(['message' => 'Operasi berhasil dilakukan.', 'icon' => 'success', 'confirmButtonText' => 'OK'])
+
+<div 
+    x-data="{ 
+        message: '{{ $message }}',
+        icon: '{{ $icon }}',
+        init() {
+            this.showAlert();
+        },
+        showAlert() {
+         console.log('Showing alert');
+            Swal.fire({
+                text: this.message,
+                icon: this.icon,
+                confirmButtonText: '{{ $confirmButtonText }}',
+                customClass: {
+                    confirmButton: 'bg-primaryColor border-0 text-white px-4 py-2 rounded-md w-96'
+                },
+                buttonsStyling: false,
+            });
+        }
+    }" 
+    x-init="init">
 </div>
