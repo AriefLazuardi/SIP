acb9b5e28397bd72fd695e58d67772ea87370ce3
Arieflazuardi <m.arieflazuardi@gmail.com>
2024-12-17 16:48:27 +0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,20 @@ import "./bootstrap";
 
 import.meta.glob(["../images/**"]);
 
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+import { Chart } from "chart.js";
+import "chart.js/auto";
+
 import Alpine from "alpinejs";
 
 import Swal from "sweetalert2";
 
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+
+window.Chart = Chart;
 window.Alpine = Alpine;
 window.Swal = Swal;
 Alpine.start();
